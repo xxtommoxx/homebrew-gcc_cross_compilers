7e468fb115a788343d0a417401271546aadff6b3
xxtommoxx <xxtommoxx@gmail.com>
2016-12-09 14:45:17 -0500

    update i386 to sha256

--- i386-elf-gcc.rb
+++ i386-elf-gcc.rb
@@ -4,7 +4,7 @@ class I386ElfGcc < Formula
   homepage 'http://gcc.gnu.org'
   url 'http://ftpmirror.gnu.org/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2'
   mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2'
-  sha1 '79dbcb09f44232822460d80b033c962c0237c6d8'
+  sha256 '2020c98295856aa13fda0f2f3a4794490757fc24bcca918d52cc8b4917b972dd'
   revision 1
 
   depends_on 'gmp'
